c389af02874d2e2cb3105953f8245763712c1aec
Hugo <martiniere.hugo.pro@gmail.com>
2025-03-05 00:54:31 +0100

    ajout de la version anglaise

--- js/main.js
+++ js/main.js
@@ -107,3 +107,57 @@
     
 })(jQuery);
 
+// Fonction pour vérifier et mettre à jour le lien du CV en fonction de la langue
+function updateCVLink() {
+    const lang = localStorage.getItem('selectedLanguage') || 'fr';  // Utilise la langue sauvegardée ou 'fr' par défaut
+
+    // Trouve le lien pour le CV
+    const cvLink = document.getElementById('cv-link');
+
+    // Vérifie si la langue est le français ou non
+    if (lang.startsWith('fr')) {
+        // Si la langue est en français, utiliser le lien du CV français
+        cvLink.href = 'img/cv/Hugo_Martiniere_CV.pdf';
+    } else {
+        // Si la langue est en anglais, utiliser le lien du CV anglais
+        cvLink.href = 'img/cv/Hugo_Martiniere_resume.pdf';
+    }
+}
+
+function changeLanguage(lang) {
+    localStorage.setItem('selectedLanguage', lang); // Sauvegarde la langue
+    document.documentElement.lang = lang;
+
+    // Mise à jour de l'icône et du texte du menu déroulant
+    const flagSrc = lang === 'fr' ? "img/langue/fr_flag.png" : "img/langue/uk_flag.png";
+    const languageText = lang === 'fr' ? "Langue" : "Language";
+    document.getElementById("languageIcon").src = flagSrc;
+    document.getElementById("languageDropdownText").textContent = languageText;
+
+    // Charger les traductions depuis le fichier JSON
+    fetch("/js/translations.json")
+        .then(response => response.json())
+        .then(data => {
+            const translations = data[lang];
+
+            // Appliquer les traductions aux éléments HTML
+            document.querySelectorAll("[data-i18n]").forEach(element => {
+                const key = element.getAttribute("data-i18n");
+                if (translations[key]) {
+                    element.textContent = translations[key];
+                }
+            });
+
+            // Met à jour le lien du CV après le changement de langue
+            updateCVLink();
+        })
+        .catch(error => console.error("Erreur de chargement des traductions :", error));
+}
+
+// Appliquer la langue sauvegardée ou par défaut au chargement
+document.addEventListener("DOMContentLoaded", function () {
+    // Si aucune langue n'est stockée, on utilise le français par défaut
+    const savedLang = localStorage.getItem('selectedLanguage') || 'fr';
+    changeLanguage(savedLang);  // Change la langue
+    updateCVLink();  // Met à jour le lien du CV en fonction de la langue
+});
